99dda9a3459851d9f348cc16ea86433a008a4750
Viswa Sunny <viswasunny007@gmail.com>
2018-08-14 10:39:35 +0530

    Create 54.java

---
+++ 54.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%2==0)
+		System.out.println(n);
+		else
+		System.out.println(n-1);
+	}
+}
