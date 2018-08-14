68a26e679756b51ab3e0bbf4b2cd31c0f1efd4ca
Viswa Sunny <viswasunny007@gmail.com>
2018-08-14 10:29:39 +0530

    Create 52.java

---
+++ 52.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n==1)
+		System.out.println("One");
+		else if(n==2)
+		System.out.println("Two");
+		else if(n==3)
+		System.out.println("Three");
+		else if(n==4)
+		System.out.println("Four");
+		else if(n==5)
+		System.out.println("Five");
+		else if(n==6)
+		System.out.println("Six");
+		else if(n==7)
+		System.out.println("Seven");
+		else if(n==8)
+		System.out.println("Eight");
+		else if(n==9)
+		System.out.println("Nine");
+		else if(n==10)
+		System.out.println("Ten");
+	}
+}
