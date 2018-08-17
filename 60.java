3271e99d27d45d086935e17a646c20827ad02654
Viswa Sunny <viswasunny007@gmail.com>
2018-08-17 09:55:25 +0530

    Create 60.java

---
+++ 60.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int k=s.nextInt();
+		int sum=0;
+		for(int i=1;i<=k;i++)
+		sum=sum+i;
+		System.out.print(sum);
+		
+		
+	}
+}
